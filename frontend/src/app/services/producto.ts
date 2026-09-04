2e433b628e74e3bba3c237aa9119ae2e20c58d24
Amaro <amaro@duocuc.cl>
2026-09-04 19:04:54 -0400

    fix: corregir producto.ts (BOM y sintaxis rota por el here-string anterior)

--- frontend/src/app/services/producto.ts
+++ frontend/src/app/services/producto.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -16,7 +16,7 @@ export interface ProductoDTO {
   providedIn: 'root'
 })
 export class ProductoService {
-  private apiUrl = ${environment.apiBaseUrl}/productos;
+  private apiUrl = environment.apiBaseUrl + '/productos';
 
   constructor(private http: HttpClient) {}
 
